cbd7fbe6364243174cff704f9a67569b2bcdf07f
JF532 <jfilipe532@hotmail.com>
2025-05-25 12:34:10 -0300

    Formulário funcionando, mas caso vocês queiram testar, no arquivo api.js vocês precisam colocar a porta certa, imagino que todo mundo seja 8080, mas caso não seja, precisa alterar

--- src/Components/UserForm/index.jsx
+++ src/Components/UserForm/index.jsx
@@ -30,24 +30,29 @@ function UserForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Nome:</label>
-      <input type="text" name="nome" value={formData.nome} onChange={handleChange} required />
+    <div className="h-full w-full bg-gray-500">
 
-      <label>Email:</label>
-      <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+      <form onSubmit={handleSubmit}>
+        <label>Nome:</label>
+        <input type="text" name="nome" value={formData.nome} onChange={handleChange} required />
 
-      <label>Senha:</label>
-      <input type="password" name="senha" value={formData.senha} onChange={handleChange} required />
+        <label>Email:</label>
+        <input type="email" name="email" value={formData.email} onChange={handleChange} required />
 
-      <label>CPF:</label>
-      <input type="text" name="cpf" value={formData.cpf} onChange={handleChange} required />
+        <label>Senha:</label>
+        <input type="password" name="senha" value={formData.senha} onChange={handleChange} required />
 
-      <label>Telefone:</label>
-      <input type="text" name="telefone" value={formData.telefone} onChange={handleChange} required />
+        <label>CPF:</label>
+        <input type="text" name="cpf" value={formData.cpf} onChange={handleChange} required />
 
-      <button type="submit">Cadastrar</button>
-    </form>
+        <label>Telefone:</label>
+        <input type="text" name="telefone" value={formData.telefone} onChange={handleChange} required />
+
+        <button type="submit" className="border-2">Cadastrar</button>
+      </form>
+
+    </div>
+
   );
 }
 
