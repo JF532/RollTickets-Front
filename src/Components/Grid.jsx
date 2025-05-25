461e63c79bc85076d34227a629d58814aec118c3
JF532 <jfilipe532@hotmail.com>
2025-05-25 16:08:42 -0300

    Teste API

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -1,27 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import Card from "./Card"
+import axios from 'axios';
+
 
 export default function Grid() {
+
+  const [filmes, setFilmes] = useState([]);
+
+  const getFilmes = () => {
+    axios({
+      method: 'get',
+      url: 'https://api.themoviedb.org/3/discover/movie',
+      params: {
+        api_key: '156d2e5ce1b6c6c0fe56949f263204e1',
+        language: 'pt-BR'
+      }
+    }).then(response => {
+      setFilmes(response.data.results);
+    })
+  }
+
+  useEffect(() => { //Com o useEffect só vai ser chamada uma vez
+    getFilmes();
+  }, []);
   return (
     <div>
       <div className='flex justify-center mt-20 items-center'>
-      
-              <div class="grid grid-cols-4 gap-4">
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-              </div>
-      
-            </div>
+
+        <div class="grid grid-cols-3 gap-4">
+          {filmes.map((movie) =>(
+            <Card key={movie.id} movie={movie}/>
+          ))}   
+        </div>
+
+      </div>
     </div>
   )
 }
