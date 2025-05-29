9a868915fd1bbda5ee8eaa6619eb236bba3d845c
JF532 <jfilipe532@hotmail.com>
2025-05-29 15:08:40 -0300

    Sidebar adicionado

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,24 +1,27 @@
-import React from "react";
-
+import { Sidebar, SidebarItem, SidebarItemGroup, SidebarItems } from "flowbite-react";
+import { LuTicket } from "react-icons/lu";
+import { CiLogout } from "react-icons/ci";
 export default function Menu({ onClose }) {
   function handleLogout() {
     localStorage.removeItem("clienteLogado");
-    window.location.href = "/login";
+    window.location.href = "/";
   }
 
   return (
-    <div className="fixed top-0 right-0 h-full w-64 bg-white text-black shadow-lg z-50 p-4">
-      <button className="text-sm mb-4 text-right w-full" onClick={onClose}>
-        ✕ Fechar
-      </button>
-      <ul className="flex flex-col gap-4">
-        <li>
-          <a href="/meus-ingressos">Meus Ingressos</a>
-        </li>
-        <li>
-          <button onClick={handleLogout}>Sair</button>
-        </li>
-      </ul>
+    <div className="fixed top-20 right-0 h-auto w-64 bg-white text-black shadow-lg z-50 rounded-full ">
+       <Sidebar aria-label="Default sidebar example" className="p-0">
+      <SidebarItems>
+        <SidebarItemGroup className="border-none ">
+          <SidebarItem href="#" icon={LuTicket}>
+            Meus Ingressos
+          </SidebarItem>
+
+          <SidebarItem  onClick={handleLogout} icon={CiLogout}>
+            Sair
+          </SidebarItem>
+        </SidebarItemGroup>
+      </SidebarItems>
+    </Sidebar>
     </div>
   );
 }
--- src/Pages/FilmesHorarios/index.jsx
+++ src/Pages/FilmesHorarios/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Grid from "../../Components/Grid";
 import Navbar from "../../Components/Navbar";
+import Footer from "../../Components/Footer";
 
 export default function FilmesHorarios() {
   return (
     <div className="w-screen min-h-screen bg-gray-900 overflow-x-hidden">
       <Navbar />
       <Grid />
+      <Footer/>
     </div>
   );
 }
