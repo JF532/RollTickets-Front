4884eccccf7066178166c652df02f4b93ea5354f
JF532 <jfilipe532@hotmail.com>
2025-05-30 16:18:57 -0300

    Correção do swiper com os filmes 'Disponiveis'

--- src/Components/Swiper.jsx
+++ src/Components/Swiper.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Swiper as Swp, SwiperSlide } from "swiper/react";
 import { EffectCoverflow, Navigation } from "swiper/modules";
+import axios from "axios";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/navigation";
@@ -10,17 +11,36 @@ import Fortnite from "../IMG/Fortnite.jpg";
 import HomemAranha from "../IMG/Homem-Aranha.jpg";
 import RickyAndMorty from "../IMG/RickyAndMorty.jpg";
 
-const filmes = [Coringa, Fortnite, HomemAranha, RickyAndMorty];
-
 export default function Swiper() {
+  const [filmes, setFilmes] = useState([]);
+
+  const getFilmes = () => {
+    axios({
+      method: "get",
+      url: "https://api.themoviedb.org/3/discover/movie",
+      params: {
+        api_key: "156d2e5ce1b6c6c0fe56949f263204e1",
+        language: "pt-BR",
+        region: "BR",
+      },
+    }).then((response) => {
+      setFilmes(response.data.results);
+      console.log(response.data.results);
+    });
+  };
+
+  useEffect(() => {
+    //Com o useEffect só vai ser chamada uma vez
+    getFilmes();
+  }, []);
   return (
     <div className="w-full h-screen bg-slate-900">
-      <div className="w-screen h-screen flex justify-center items-center px-20">
+      <div className="w-screen h-screen flex justify-center items-center mt-19">
         <Swp
           effect="coverflow"
           grabCursor={true}
           centeredSlides={true}
-          slidesPerView={2}
+          slidesPerView={4}
           loop={true}
           navigation={true}
           coverflowEffect={{
@@ -31,16 +51,17 @@ export default function Swiper() {
             slideShadows: true,
           }}
           modules={[EffectCoverflow, Navigation]}
-          className="mySwiper"
+          className="mySwiper "
         >
           {filmes.map((filme, index) => (
             <SwiperSlide key={index}>
               <img
-                src={filme}
-                className="w-[750px] h-[550px] object-cover rounded-3xl shadow-xl/30"
+                width={500}
+                height={500}
+                src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`}
+                alt="image 1"
+                className="w-[383px] h-[574px] object-cover rounded-3xl shadow-xl/30"
               />
-
-              
             </SwiperSlide>
           ))}
         </Swp>
