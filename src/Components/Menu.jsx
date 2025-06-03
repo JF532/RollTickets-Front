7adb06d9117020e0432662e91f50e903311bdd22
JF532 <jfilipe532@hotmail.com>
2025-06-03 13:40:49 -0300

    Algumas estilizações do 'Menu'

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -24,23 +24,23 @@ export default function Menu({ onClose }) {
   }
 
   return (
-    <div className="fixed top-20 right-0 h-auto w-64 bg-white text-black shadow-lg z-50 rounded-full ">
+    <div className="absolute top-20 right-0 h-auto w-64 bg-white text-gray-300 shadow-lg z-50 rounded-full ">
       <Sidebar aria-label="Default sidebar example" className="p-0">
         <SidebarItems>
           <SidebarItemGroup className="border-none ">
-            <div className="flex flex-col">
-              <span className=" text-white">{cliente?.nome}</span>
-              <span className="text-white">{cliente?.email}</span>
+            <div className="flex flex-col gap-3">
+              <span className=" text-purple-300"><span className="text-gray-300">Usuário:</span> {cliente?.nome}</span>
+              <span className="text-purple-300"><span className="text-gray-300">Email:</span> {cliente?.email}</span>
             </div>
 
-            <SidebarItem href="#" icon={LuTicket}>
+            <SidebarItem className="hover:text-purple-300 transition delay-100 duration-300 ease-in " href="#" icon={LuTicket}>
               Meus Ingressos
             </SidebarItem>
 
             <SidebarItem
               onClick={handleLogout}
               icon={CiLogout}
-              className="cursor-pointer"
+              className="hover:text-purple-300 transition delay-100 duration-300 ease-in cursor-pointer"
             >
               Sair
             </SidebarItem>
