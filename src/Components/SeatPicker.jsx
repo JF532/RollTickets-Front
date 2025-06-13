bce4093c2ddeef7b938c5208b254734b2961f557
JF532 <jfilipe532@hotmail.com>
2025-06-13 17:05:49 -0300

    Tela dos assentos modificada

--- src/Components/SeatPicker.jsx
+++ src/Components/SeatPicker.jsx
@@ -1,16 +1,52 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { MdOutlineEventSeat } from "react-icons/md";
 
-export default function SeatPicker({ capacidade, setAssento }) {
+export default function SeatPicker({
+  capacidade,
+  setAssento,
+  assentosReservados = [],
+  sessaoSelecionada,
+  atualizarAssentosReservados,
+}) {
   // Gera array com números de 1 até a capacidade da sala
   const assentos = Array.from({ length: capacidade }, (_, i) => i + 1);
 
-  const [assentosSelecionados, setAssentosSelecionados] = useState([]);
+  const [assentosSelecionados, setAssentosSelecionados] = useState([]); //Serve para armazenas os assentos que o usuário armazenou
+
+  const [mensagem, setMensagem] = useState(""); //Serve só para armazenar a mensagem para o usuário
 
   const handleClick = (num) => {
-    setAssentosSelecionados((prev) =>
-      prev.includes(num) ? prev.filter((n) => n !== num) : [...prev, num]
+    setAssentosSelecionados(
+      (
+        prev //Atualiza o assentosSelecionados com a lista
+      ) =>
+        prev.some((a) => a.numero === num)
+          ? prev.filter((a) => a.numero !== num)
+          : [...prev, { numero: num, fileira: "A" }] //Se o assento já estiver selecionado remove ele da lista "filter", caso ele não esteja, adiciona ele na lista
     );
     setAssento(num); // Se quiser passar só o último assento selecionado
+  };
+
+  const confirmarReserva = async () => {
+    try {
+      for (let numero of assentosSelecionados) {
+        //Percorre assentosSelecionados e manda isso aqui de baixo para o back
+        await axios.post("http://localhost:8080/api/assentos/reservar", {
+          numero: numero.toString(),
+          fileira: "A",
+          sala: { id: sessaoSelecionada.sala.id },
+          sessao: { id: sessaoSelecionada.id },
+        });
+      }
+
+      setMensagem("Reserva realizada com sucesso!");
+      setAssentosSelecionados([]); //Limpa os assentos selecionados
+      atualizarAssentosReservados(); //Chama essa função para atualizar os assentos que estão ocupados
+    } catch (error) {
+      console.error(error);
+      setMensagem("Erro ao reservar assentos.");
+    }
   };
 
   useEffect(() => {
@@ -18,21 +54,65 @@ export default function SeatPicker({ capacidade, setAssento }) {
   }, [assentosSelecionados]);
 
   return (
-    <div className="max-w-md mx-auto p-4 bg-gray-900 rounded-md text-white grid grid-cols-5 gap-4">
-      {assentos.map((num) => (
-        <button
-          key={num}
-          type="button"
-          onClick={() => handleClick(num)}
-          className={`border p-2 rounded ${
-            assentosSelecionados.includes(num)
-              ? "bg-green-600"
-              : "bg-gray-800 hover:bg-gray-700"
-          }`}
-        >
-          {num}
-        </button>
-      ))}
+    <div className="w-auto h-auto mt-40 ml-20 p-4 bg-gray-900 rounded-md text-white">
+     
+     <h2 className="w-100 ml-20 text-center text-2xl mb-10 font-semibold">
+        Selecione seus assentos
+      </h2>
+
+      <div className="flex  items-center-safe">
+        <div className="grid grid-cols-8 gap-4 mb-4">
+          {assentos.map((num) => {
+            const ocupado = assentosReservados.includes(num); //Verifica se o assento está ocupado
+            const selecionado = assentosSelecionados.some(
+              (a) => a.numero === num
+            ); //Verifica se o assento está selecionado
+
+            return (
+              <button
+                key={num}
+                type="button"
+                onClick={() => !ocupado && handleClick(num)} //Só chama handleClick se o assento n estiver ocupado
+                disabled={ocupado} //Se estiver ocupado desativa o botão
+                className={`w-14 h-14 text-xl flex items-center justify-center border-2 border-black p-2 rounded ${
+                  ocupado
+                    ? "bg-red-700 cursor-not-allowed" //Cor do botão se ele estiver ocupado
+                    : selecionado
+                    ? "bg-[#81318a]  cursor-pointer" //Cor do botão se ele for selecionado
+                    : "bg-gray-800 hover:bg-gray-700  cursor-pointer" //Cor se ele estiver livre
+                }`}
+              >
+                <MdOutlineEventSeat />{" "}
+                {/* Numero vai aparecer dentro do botão */}
+              </button>
+            );
+          })}
+        </div>
+        {assentosSelecionados.length > 0 && (
+          <div className=" ml-40 border-2 border-[#81318a] rounded p-10">
+            <ul className="">
+              <h1 className="mb-3 text-[#793381] font-semibold">
+                Ingressos Selecionados:
+              </h1>
+              {assentosSelecionados.map((a) => (
+                <li className="text-gray-300" key={a.numero}>
+                  Fileira <span className="text-[#b966c2]">{a.fileira}</span> -
+                  Assento <span className="text-[#81318a]">{a.numero}</span>
+                </li>
+              ))}
+            </ul>
+
+            <button
+              onClick={confirmarReserva}
+              className="w-full  bg-[#81318a] hover:bg-[#b966c2] text-white mt-6 py-2 px-4 rounded"
+            >
+              Confirmar Reserva
+            </button>
+          </div>
+        )}
+        {mensagem && <p className="mt-2 text-center text-sm">{mensagem}</p>}{" "}
+        {/* Exibi uma mensagem que a gente colocou lá em cima */}
+      </div>
     </div>
   );
 }
--- src/Pages/Compra/index.jsx
+++ src/Pages/Compra/index.jsx
@@ -10,10 +10,13 @@ import Dropdown from "../../Components/Dropdown";
 export default function Compra() {
   const [filme, setFilme] = useState(null);
   const [sessoes, setSessoes] = useState([]);
-  const [sessaoSelecionada, setSessaoSelecionada] = useState(null);
-  const [assento, setAssento] = useState([]);
+  const [sessaoSelecionada, setSessaoSelecionada] = useState(null); //Objeto da sessão que foi escolhida pelo usuário
+  const [assento, setAssento] = useState([]); //Array para guardar os assentos selecionados
+  const [assentosReservados, setAssentosReservados] = useState([]); //Guarda quais assentos já foram reservados
 
   const { id } = useParams();
+
+  
 
   useEffect(() => {
     axios
@@ -26,6 +29,22 @@ export default function Compra() {
       .then((response) => setSessoes(response.data))
       .catch((error) => console.error("Erro ao buscar sessões", error));
   }, [id]);
+
+  const buscarAssentosReservados = () => { //Função para buscar assentos reservados, ou seja, os assentos que estão no back
+    if (!sessaoSelecionada) return; //Só executa se uma sessão tiver sido executada
+
+    axios
+      .get(`http://localhost:8080/api/assentos/reservados/${sessaoSelecionada.id}`)
+      .then((response) => {
+        const ocupados = response.data.map((assento) => Number(assento.numero)); // Retorna do get os assentos que estão ocupados e atualizam o assentosReservados com esses valores
+        setAssentosReservados(ocupados);
+      })
+      .catch(() => setAssentosReservados([]));
+  };
+
+  useEffect(() => {
+    buscarAssentosReservados();
+  }, [sessaoSelecionada]);
 
   console.log(sessaoSelecionada);
   return (
@@ -47,6 +66,9 @@ export default function Compra() {
             <SeatPicker
               setAssento={setAssento}
               capacidade={sessaoSelecionada?.sala?.capacidade}
+              assentosReservados={assentosReservados}
+              sessaoSelecionada={sessaoSelecionada}
+              atualizarAssentosReservados={buscarAssentosReservados}
             />
           )}
         </div>
