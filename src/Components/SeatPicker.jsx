5b54ab16a6eadf4ec88ec23c87a2613d23ea651c
JF532 <jfilipe532@hotmail.com>
2025-06-17 14:30:30 -0300

    Configurações 2

--- src/Components/SeatPicker.jsx
+++ src/Components/SeatPicker.jsx
@@ -10,7 +10,7 @@ export default function SeatPicker({
   atualizarAssentosReservados,
   filme
 }) {
-  // Gera array com números de 1 até a capacidade da sala
+  // Gera array com números de 1 até a capacidade das salas
   const assentos = [];
 
   const fileiras = ["A", "B", "C", "D", "E", "F", "G","H"];
